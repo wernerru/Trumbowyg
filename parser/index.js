a3c7d6cc18ed34dee184f68808cb146e41d21a3c
Alex-D <contact@alex-d.fr>
2019-11-17 04:50:34 +0100

    --wip-- [skip ci]

---
+++ parser/index.js
@@ -0,0 +1,52 @@
+const input = document.getElementById('input')
+
+const blockToTag = {
+  paragraph: 'p',
+  div: 'div',
+  ul: 'ul',
+  li: 'li',
+  table: 'table',
+  tableRow: 'tr',
+  tableCell: 'td',
+  strong: 'strong',
+  emphasis: 'em',
+  delete: 'del',
+  underline: 'u',
+  heading: 'h1',
+}
+
+const tagByBlock = Object.entries(blockToTag).reduce((acc, entry) => {
+  const [key, value] = entry
+  acc[value] = key
+  return acc
+}, {})
+
+function parse(node) {
+  return Array.from(node.childNodes).map((subNode) => {
+    const type = subNode.nodeType === Node.TEXT_NODE
+      ? 'text'
+      : tagByBlock[subNode.tagName.toLowerCase()]
+
+    let nodeStruct = { type }
+
+    if (subNode.nodeType === Node.TEXT_NODE) {
+      nodeStruct.value = subNode.textContent
+    } else if (subNode.childNodes.length > 0) {
+      nodeStruct.children = parse(subNode)
+    }
+
+    return nodeStruct
+  })
+}
+
+const output = parse(input)
+console.log(output)
+
+document.getElementById('output').innerHTML = JSON.stringify(output, null, 2)
+hljs.initHighlightingOnLoad()
+
+document.getElementById('input').addEventListener('input', function () {
+  document.getElementById('output').innerHTML = JSON.stringify(parse(this), null, 2)
+  hljs.initHighlighting.called = false
+  hljs.initHighlightingOnLoad()
+})
